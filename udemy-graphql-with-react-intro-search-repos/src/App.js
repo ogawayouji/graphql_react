ad8c7ed2301a0636ed66465f0ec2b1774310c8a9
ogawayouji <satsumaimo697@gmail.com>
2021-09-22 23:17:39 +0900

    chapger29, 30

--- udemy-graphql-with-react-intro-search-repos/src/App.js
+++ udemy-graphql-with-react-intro-search-repos/src/App.js
@@ -15,9 +15,11 @@ import { ME, SEARCH_REPOSITORIES } from './graphql'
 //   }
 // `
 
+const PER_PAGE = 5 
 const DEFAULT_STATE = {
   // const VARIABLES = {
-  first: 5,
+  first: PER_PAGE,
+  // first: 5,
   after: null,
   last: null,
   before: null,
@@ -40,6 +42,28 @@ class App extends Component {
       query: event.target.value
     })
   }
+
+  handleSubmit(event) {
+    event.preventDefault()
+  }
+
+  goNext(search) {
+    this.setState({ 
+      first: PER_PAGE,
+      after: search.pageInfo.endCursor,
+      last: null,
+      before: null
+    })
+  }
+  goPrevious(search) {
+    this.setState({ 
+      first: null,
+      after: null,
+      last: PER_PAGE,
+      before: search.pageInfo.startCursor
+    })
+  }
+
   render() {
     const { query, first, last, before, after } = this.state
     console.log({query})
@@ -69,7 +93,47 @@ class App extends Component {
                 // console.log(data.search)
                 // console.log({data})
               // return <div>GitHub Repositories search Results - {data.search.repositoryCount} Repositories</div>
-              return <h2>{title}</h2>
+              return (
+                <React.Fragment>
+                  <h2>{title}</h2>
+                  <ul>
+                    {
+                      // console.log(search.edges)
+                      search.edges.map(edge => {
+                        const node = edge.node  
+                        return (
+                          <li key={node.id}>
+                            <a href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
+                  {
+                    search.pageInfo.hasPreviousPage === true ?
+                    <button
+                    onClick={
+                      this.goPrevious.bind(this, search)
+                    }>
+                      Previous
+                    </button>
+                    :
+                    null
+                  }
+                  {
+                    search.pageInfo.hasNextPage === true ?
+                    <button
+                    onClick={
+                      this.goNext.bind(this, search)
+                    }>
+                      next
+                    </button>
+                    :
+                    null
+                  }
+
+                </React.Fragment>
+              )
                 // return <div></div>
               }
             }
